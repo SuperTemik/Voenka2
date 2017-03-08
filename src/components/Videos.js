b279a7da63ddcec035e8140650d2c7b5b6c5739c
SuperTemik <sukmanovartem@mail.ru>
2017-03-08 21:18:01 +0300

    Update Videos.js

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,22 +1,18 @@
 const React = require('react');
 const Video = require('components/Video');
 
-const purpose = require('../../static/videos/purpose.mp4');
-const common = require('../../static/videos/common.mp4');
-const drl = require('../../static/videos/drl.mp4');
-const prl = require('../../static/videos/prl.mp4');
+const РТО = require('../../static/videos/РТО.mp4');
 
 const Videos = React.createClass({
   render() {
     return (
       <div className="container">
         <div className="row">
-          <Video src={purpose} title="Назначение и состав"/>
-          <Video src={common} title="Общее расположение"/>
+          <Video src={РТО} title="Назначение и состав"/>
         </div>
         <div className="row">
-          <Video src={drl} title="ДРЛ ТТХ"/>
-          <Video src={prl} title="ПРЛ ТТХ"/>
+          //<Video src={drl} title="ДРЛ ТТХ"/>
+          //<Video src={prl} title="ПРЛ ТТХ"/>
         </div>
       </div>
     );
